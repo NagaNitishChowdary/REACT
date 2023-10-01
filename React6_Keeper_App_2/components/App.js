9ed83783fb3a5127b5b36e89cc94313bf613d214
Naga Nitish <95007559+NagaNitishChowdary@users.noreply.github.com>
2023-10-01 16:56:08 +0530

    Create App.js

---
+++ React6_Keeper_App_2/components/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+// console.log(notes) ;
+// function createNote(noteItem) {
+//   return <Note key={noteItem.key} title={noteItem.title} content={noteItem.content} />;
+// }
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {/* {notes.map(createNote)} */}
+      {notes.map((noteItem) => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
