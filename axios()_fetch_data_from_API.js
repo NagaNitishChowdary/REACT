eba6605ae155b34b77a42a3df85d4b2aa8580aea
Naga Nitish <naganitishkapu@gmail.com>
2024-02-21 15:46:15 +0530

    Create axios()_fetch_data_from_API.js

---
+++ axios()_fetch_data_from_API.js
@@ -0,0 +1,19 @@
+import React,{useEffect,useState} from 'react'
+import axios from 'axios' ;
+
+const App = () =>{
+
+  const [data,setData] = useState([]) ; 
+
+  useEffect(() => {axios.get("https://jsonplaceholder.typicode.com/todos").then(
+    res => setData(res.data)
+  )} ,[]) ;
+
+  return (
+    <div>
+      {data.map(item => <li key={item.id}>{item.title}</li>)}
+    </div>
+  )
+}
+
+export default App
